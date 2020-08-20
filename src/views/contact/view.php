941bcb13e045730a2b844aa8ae36dbe45a90b763
Mong Dao <mongdaovan86@yahoo.com>
2020-08-20 19:01:44 +0700

    update 20082020

--- src/views/contact/view.php
+++ src/views/contact/view.php
@@ -22,21 +22,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <!-- Title -->
     <div class="hk-pg-header">
         <h4 class="hk-pg-title"><span class="pg-title-icon"><span
-                        class="ion ion-md-apps"></span></span><?= Html::encode($this->title) ?>
+                        class="ion ion-md-apps"></span></span><?=ContactModule::t('contact', 'Chi tiết'); ?>: <?= Html::encode($this->title) ?>
         </h4>
-        <p>
-            <a class="btn btn-outline-light" href="<?= Url::to(['create']); ?>"
-                title="<?= ContactModule::t('contact', 'Create'); ?>">
-                <i class="fa fa-plus"></i> <?= ContactModule::t('contact', 'Create'); ?></a>
-            <?= Html::a(ContactModule::t('contact', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-            <?= Html::a(ContactModule::t('contact', 'Delete'), ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => ContactModule::t('contact', 'Are you sure you want to delete this item?'),
-                    'method' => 'post',
-                ],
-            ]) ?>
-        </p>
     </div>
     <!-- /Title -->
 
@@ -61,7 +48,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                 return Yii::$app->getModule('contact')->params['status'][$model->status];
                             }
                         ],
-						'created_at:datetime',x
+						'created_at:datetime',
                     ],
                 ]) ?>
             </section>
